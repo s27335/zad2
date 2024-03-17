14aec8380c3767fddc39549ac56311fc428c997d
es27335 <s27335@pjwstk.edu.pl>
2024-03-17 18:21:58 +0100

    Max added

--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -8,7 +8,8 @@
         Console.WriteLine("Modification3");
 
         int[] tab = { 1, 6, 7, 4, 3, 2, 5, 6 };
-        Console.Write(Avg(tab));
+        Console.WriteLine(Avg(tab));
+        Console.WriteLine((Max(tab)));
     }
 
     
@@ -21,6 +22,20 @@
         }
         
         return suma/tab.Length;   
+    }
+
+    public static int Max(int[] tab)
+    {
+        int max = 0;
+        for (int i = 0; i < tab.Length; i++)
+        {
+            if (tab[i]>max)
+            {
+                max = tab[i];
+            }
+        }
+
+        return max;
     }
 
 }
