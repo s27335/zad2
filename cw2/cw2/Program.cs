bf09512a109f3c934b5e1099f4d97fd7057c6c7d
es27335 <s27335@pjwstk.edu.pl>
2024-03-17 17:35:09 +0100

    New Task

--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -1,4 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Modification1");
-Console.WriteLine("Modification2");
-Console.WriteLine("Modification3");
+﻿public class Program{
+
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Modification1");
+        Console.WriteLine("Modification2");
+        Console.WriteLine("Modification3");
+    }
+
+    
+    public static double Avg()
+    {
+        return 0;
+    }
+
+}
